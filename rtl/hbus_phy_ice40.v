6ef2eb6119a20bdfbe835c25e038e81531daf675
Sylvain Munaut <tnt@246tNt.com>
2021-08-18 00:03:17 +0200

    rtl: Reformat code to align ports in instanciation
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- rtl/hbus_phy_ice40.v
+++ rtl/hbus_phy_ice40.v
@@ -114,24 +114,24 @@ module hbus_phy_ice40 #(
 	// -----
 
 	ice40_oserdes #(
-		.MODE("CLK90_2X"),
-		.SERDES_GRP(SERDES_GRP_BASE + 'h90)
+		.MODE       ("CLK90_2X"),
+		.SERDES_GRP (SERDES_GRP_BASE + 'h90)
 	) oserdes_ck_I (
-		.d({2'b00, phy_ck_en}),
-		.q(serdes_ck_dout),
-		.sync(sync_4x),
-		.clk_1x(clk_1x),
-		.clk_4x(clk_4x)
+		.d      ({2'b00, phy_ck_en}),
+		.q      (serdes_ck_dout),
+		.sync   (sync_4x),
+		.clk_1x (clk_1x),
+		.clk_4x (clk_4x)
 	);
 
 	SB_IO #(
 		.PIN_TYPE(6'b1100_01)
 	) io_ck_I (
-		.PACKAGE_PIN(hbus_ck),
-		.OUTPUT_ENABLE(1'b1),
-		.D_OUT_0(serdes_ck_dout[0]),
-		.D_OUT_1(serdes_ck_dout[1]),
-		.OUTPUT_CLK(clk_4x)
+		.PACKAGE_PIN   (hbus_ck),
+		.OUTPUT_ENABLE (1'b1),
+		.D_OUT_0       (serdes_ck_dout[0]),
+		.D_OUT_1       (serdes_ck_dout[1]),
+		.OUTPUT_CLK    (clk_4x)
 	);
 
 
@@ -139,51 +139,51 @@ module hbus_phy_ice40 #(
 	// ----
 
 	ice40_oserdes #(
-		.MODE("DATA"),
-		.SERDES_GRP(SERDES_GRP_BASE + 'h80)
+		.MODE       ("DATA"),
+		.SERDES_GRP (SERDES_GRP_BASE + 'h80)
 	) oserdes_rwds_o_I (
-		.d(phy_rwds_out),
-		.q(serdes_rwds_dout),
-		.sync(sync_4x),
-		.clk_1x(clk_1x),
-		.clk_4x(clk_4x)
+		.d      (phy_rwds_out),
+		.q      (serdes_rwds_dout),
+		.sync   (sync_4x),
+		.clk_1x (clk_1x),
+		.clk_4x (clk_4x)
 	);
 
 	ice40_oserdes #(
-		.MODE("DATA"),
-		.SERDES_GRP(SERDES_GRP_BASE + 'h81)
+		.MODE       ("DATA"),
+		.SERDES_GRP (SERDES_GRP_BASE + 'h81)
 	) oserdes_rwds_oe_I (
-		.d({phy_rwds_oe[1], phy_rwds_oe[1], phy_rwds_oe[0], phy_rwds_oe[0]}),
-		.q(serdes_rwds_oe),
-		.sync(sync_4x),
-		.clk_1x(clk_1x),
-		.clk_4x(clk_4x)
+		.d      ({phy_rwds_oe[1], phy_rwds_oe[1], phy_rwds_oe[0], phy_rwds_oe[0]}),
+		.q      (serdes_rwds_oe),
+		.sync   (sync_4x),
+		.clk_1x (clk_1x),
+		.clk_4x (clk_4x)
 	);
 
 	ice40_iserdes #(
-		.EDGE_SEL("DUAL_POS_POS"),
-		.PHASE_SEL("DYNAMIC"),
-		.SERDES_GRP(SERDES_GRP_BASE + 'h80)
+		.EDGE_SEL   ("DUAL_POS_POS"),
+		.PHASE_SEL  ("DYNAMIC"),
+		.SERDES_GRP (SERDES_GRP_BASE + 'h80)
 	) iserdes_rwds_I (
-		.d(serdes_rwds_din),
-		.q(phy_rwds_in),
-		.edge_sel(phy_edge),
-		.phase_sel(phy_phase),
-		.sync(sync_rd),
-		.clk_1x(clk_1x),
-		.clk_4x(clk_rd)
+		.d         (serdes_rwds_din),
+		.q         (phy_rwds_in),
+		.edge_sel  (phy_edge),
+		.phase_sel (phy_phase),
+		.sync      (sync_rd),
+		.clk_1x    (clk_1x),
+		.clk_4x    (clk_rd)
 	);
 
 	SB_IO #(
 		.PIN_TYPE(6'b 1101_00)
 	) io_rwds_I (
-		.PACKAGE_PIN(hbus_rwds),
-		.OUTPUT_ENABLE(serdes_rwds_oe[0]),
-		.D_OUT_0(serdes_rwds_dout[0]),
-		.D_IN_0(serdes_rwds_din[0]),
-		.D_IN_1(serdes_rwds_din[1]),
-		.OUTPUT_CLK(clk_4x),
-		.INPUT_CLK(clk_rd)
+		.PACKAGE_PIN   (hbus_rwds),
+		.OUTPUT_ENABLE (serdes_rwds_oe[0]),
+		.D_OUT_0       (serdes_rwds_dout[0]),
+		.D_IN_0        (serdes_rwds_din[0]),
+		.D_IN_1        (serdes_rwds_din[1]),
+		.OUTPUT_CLK    (clk_4x),
+		.INPUT_CLK     (clk_rd)
 	);
 
 
@@ -197,51 +197,51 @@ module hbus_phy_ice40 #(
 		begin
 
 			ice40_oserdes #(
-				.MODE("DATA"),
-				.SERDES_GRP(SERDES_GRP_BASE + (i<<4))
+				.MODE       ("DATA"),
+				.SERDES_GRP (SERDES_GRP_BASE + (i<<4))
 			) oserdes_dq_o_I (
-				.d({phy_dq_out[24+i], phy_dq_out[16+i], phy_dq_out[8+i], phy_dq_out[i]}),
-				.q(serdes_dq_dout[i]),
-				.sync(sync_4x),
-				.clk_1x(clk_1x),
-				.clk_4x(clk_4x)
+				.d      ({phy_dq_out[24+i], phy_dq_out[16+i], phy_dq_out[8+i], phy_dq_out[i]}),
+				.q      (serdes_dq_dout[i]),
+				.sync   (sync_4x),
+				.clk_1x (clk_1x),
+				.clk_4x (clk_4x)
 			);
 
 			ice40_oserdes #(
-				.MODE("DATA"),
-				.SERDES_GRP(SERDES_GRP_BASE + (i<<4) + 1)
+				.MODE       ("DATA"),
+				.SERDES_GRP (SERDES_GRP_BASE + (i<<4) + 1)
 			) oserdes_dq_oe_I (
-				.d({phy_dq_oe[1], phy_dq_oe[1], phy_dq_oe[0], phy_dq_oe[0]}),
-				.q(serdes_dq_oe[i]),
-				.sync(sync_4x),
-				.clk_1x(clk_1x),
-				.clk_4x(clk_4x)
+				.d      ({phy_dq_oe[1], phy_dq_oe[1], phy_dq_oe[0], phy_dq_oe[0]}),
+				.q      (serdes_dq_oe[i]),
+				.sync   (sync_4x),
+				.clk_1x (clk_1x),
+				.clk_4x (clk_4x)
 			);
 
 			ice40_iserdes #(
-				.EDGE_SEL("DUAL_POS_POS"),
-				.PHASE_SEL("DYNAMIC"),
-				.SERDES_GRP(SERDES_GRP_BASE + (i<<4))
+				.EDGE_SEL   ("DUAL_POS_POS"),
+				.PHASE_SEL  ("DYNAMIC"),
+				.SERDES_GRP (SERDES_GRP_BASE + (i<<4))
 			) iserdes_dq_I (
-				.d(serdes_dq_din[i]),
-				.q({phy_dq_in[24+i], phy_dq_in[16+i], phy_dq_in[8+i], phy_dq_in[i]}),
-				.edge_sel(phy_edge),
-				.phase_sel(phy_phase),
-				.sync(sync_rd),
-				.clk_1x(clk_1x),
-				.clk_4x(clk_rd)
+				.d          (serdes_dq_din[i]),
+				.q          ({phy_dq_in[24+i], phy_dq_in[16+i], phy_dq_in[8+i], phy_dq_in[i]}),
+				.edge_sel   (phy_edge),
+				.phase_sel  (phy_phase),
+				.sync       (sync_rd),
+				.clk_1x     (clk_1x),
+				.clk_4x     (clk_rd)
 			);
 
 			SB_IO #(
 				.PIN_TYPE(6'b 1101_00)
 			) io_dq_I (
-				.PACKAGE_PIN(hbus_dq[i]),
-				.OUTPUT_ENABLE(serdes_dq_oe[i][0]),
-				.D_OUT_0(serdes_dq_dout[i][0]),
-				.D_IN_0(serdes_dq_din[i][0]),
-				.D_IN_1(serdes_dq_din[i][1]),
-				.OUTPUT_CLK(clk_4x),
-				.INPUT_CLK(clk_rd)
+				.PACKAGE_PIN   (hbus_dq[i]),
+				.OUTPUT_ENABLE (serdes_dq_oe[i][0]),
+				.D_OUT_0       (serdes_dq_dout[i][0]),
+				.D_IN_0        (serdes_dq_din[i][0]),
+				.D_IN_1        (serdes_dq_din[i][1]),
+				.OUTPUT_CLK    (clk_4x),
+				.INPUT_CLK     (clk_rd)
 			);
 
 		end
@@ -257,10 +257,10 @@ module hbus_phy_ice40 #(
 	SB_IO #(
 		.PIN_TYPE(6'b 1101_01)
 	) io_cs_n_I[3:0] (
-		.PACKAGE_PIN(hbus_cs_n),
-		.OUTPUT_ENABLE(1'b1),
-		.D_OUT_0(iob_cs_n),
-		.OUTPUT_CLK(clk_4x)
+		.PACKAGE_PIN   (hbus_cs_n),
+		.OUTPUT_ENABLE (1'b1),
+		.D_OUT_0       (iob_cs_n),
+		.OUTPUT_CLK    (clk_4x)
 	);
 
 	assign hbus_rst_n = phy_rst_n;
